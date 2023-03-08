860042412152c7911bb4316cea4eb6dec9463267
Josh Randall <josh-randall@hush.ai>
2023-03-08 12:18:31 -0500

    Illustrate <p> bug.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Comp from '$routes/comp.svelte'
+</script>
+<p>
+  (Before Comp)
+  <Comp>
+    <svelte:fragment slot="hidden"><p>This should be visible</p></svelte:fragment>
+    <svelte:fragment slot="alsoHidden"><p>This should be visible</p></svelte:fragment>
+    <div>
+      <section>
+        <span>
+          This should not!
+        </span>
+      </section>
+    </div>
+  </Comp>
+  (After Comp)
+</p>
---
+++ src/routes/comp.svelte
@@ -0,0 +1,9 @@
+<span style="display:none;">
+  <slot />
+</span>
+<span style="display:none;">
+  <slot name="hidden" />
+</span>
+<span style="display:none;">
+  <slot name="alsoHidden" />
+</span>
